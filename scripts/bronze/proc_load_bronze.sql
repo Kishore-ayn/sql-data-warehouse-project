1e5b021e7d10cc961da2e1ce143beb1d483886b2
Kishore Senapathi <162533889+Kishore-ayn@users.noreply.github.com>
2025-04-25 23:24:31 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,19 +1,19 @@
 /*
 ===============================================================================
-Stored Procedure ; Load Bronze Layer (Source --> Bronze)
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ===============================================================================
-Script Purpose :
-	This stored procedure loads the data into bronze schema from external CSV files
-	It performs the following actions :
-	- Truncate the bronze tables before loading data.
-	- Uses the 'BULK INSERT' command to load data from CSV files to bronze tables.
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
-Parameters :
-	None.
-	This stored procedure does not accept any parameters or return values
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
 
-Usage Example :
-	EXEC bronze.load_bronze;
+Usage Example:
+    EXEC bronze.load_bronze;
 ===============================================================================
 */
 
@@ -39,7 +39,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -55,7 +55,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -70,7 +70,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -89,7 +89,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\sql\dwh_project\datasets\source_erp\loc_a101.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -104,7 +104,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -119,7 +119,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'D:\PERSONAL\DE\Projects\Dataware-Housing-Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -144,8 +144,3 @@ BEGIN
 		PRINT '=========================================='
 	END CATCH
 END
-
-GO
-
-
-
